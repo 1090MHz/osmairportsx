98cec18c82f0e681aa729dd495a54ab86da0211c
unknown <girivs@WIN-QPPKTJO58PO.(none)>
2013-09-22 19:31:41 -0400

    Windows assembly manifest for new UI look

--- setup.py
+++ setup.py
@@ -36,6 +36,40 @@ if sys.platform == 'darwin':
 elif sys.platform == 'win32':
     import GUI.py2exe
     import py2exe
+	
+    manifest = '''
+<?xml version='1.0' encoding='UTF-8' standalone='yes'?>
+<assembly xmlns='urn:schemas-microsoft-com:asm.v1' manifestVersion='1.0'>
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel level='asInvoker' uiAccess='false' />
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+  <dependency>
+    <dependentAssembly>
+      <assemblyIdentity
+     type='win32'
+     name='Microsoft.VC90.CRT'
+     version='9.0.21022.8'
+     processorArchitecture='*'
+     publicKeyToken='1fc8b3b9a1e18e3b' />
+    </dependentAssembly>
+  </dependency>
+  <dependency>
+    <dependentAssembly>
+      <assemblyIdentity
+         type="win32"
+         name="Microsoft.Windows.Common-Controls"
+         version="6.0.0.0"
+         processorArchitecture="*"
+         publicKeyToken="6595b64144ccf1df"
+         language="*" />
+    </dependentAssembly>
+  </dependency>
+</assembly>
+    '''
     extra_options = dict(
                          setup_requires=['py2exe'],
                          options = dict(
@@ -46,7 +80,10 @@ elif sys.platform == 'win32':
     setup(
         name='OSMAirportsX',
         app=['OSMAirportsX.py'],
-        windows=['OSMAirportsX.py'],
+        windows=[dict(
+			script='OSMAirportsX.py',
+			other_resources= [(24,1,manifest)]
+			)],
         data_files=[
             'airports.csv',
             'runways.csv',
